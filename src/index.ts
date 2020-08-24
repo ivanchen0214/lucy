4b6b10a1709b59bee8aa55ca69a814283906d87e
chenjiahau <ivan.chen@dlinkcorp.com>
2020-08-24 08:21:58 +0800

    Change

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('asdfdd');
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+
+const numbersCollection = new NumbersCollection([10, 3, 11, 0]);
+const sorter = new Sorter(numbersCollection);
+
+sorter.sort();
+console.log(sorter.collection);
