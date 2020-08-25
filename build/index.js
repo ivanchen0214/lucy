964d9d5f328c71a64ea758a87f392c4c49f76494
chenjiahau <ivan.chen@dlinkcorp.com>
2020-08-25 08:14:39 +0800

    Change

--- build/index.js
+++ build/index.js
@@ -1,9 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
-var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 // const numbersCollection = new NumbersCollection([10, 3, 11, 0]);
-var charactersCollection = new CharactersCollection_1.CharactersCollection('bcdsa');
-var sorter = new Sorter_1.Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('bcdsa');
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(50);
+linkedList.add(100);
+linkedList.add(-5);
+linkedList.add(-1);
+var sorter = new Sorter_1.Sorter(linkedList);
 sorter.sort();
-console.log(sorter.collection);
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([10, 3, 11, 0]);
-const charactersCollection = new CharactersCollection('bcdsa');
+// const charactersCollection = new CharactersCollection('bcdsa');
+const linkedList = new LinkedList();
+linkedList.add(50);
+linkedList.add(100);
+linkedList.add(-5);
+linkedList.add(-1);
 
-const sorter = new Sorter(charactersCollection);
+const sorter = new Sorter(linkedList);
 
 sorter.sort();
-console.log(sorter.collection);
+linkedList.print();
