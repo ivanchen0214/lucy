20ecff469701743b6cdd2c3d4256c9acaab3312c
chenjiahau <ivan.chen@dlinkcorp.com>
2020-09-03 19:51:58 +0800

    Change

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,25 @@
 import { Eventing } from './Eventing';
+import { Sync } from './Sync';
 
-interface userProp {
+export interface UserProp {
   id?: number;
   name?: string;
   age?: number;
 };
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProp> = new Sync<UserProp>(rootUrl);
 
-  constructor(private data: userProp) { }
+  constructor(private data: UserProp) { }
 
   get(propName: string): (string | number) {
     return this.data[propName];
   }
 
-  set(update: userProp): void {
+  set(update: UserProp): void {
     Object.assign(this.data, update);
   }
 }
