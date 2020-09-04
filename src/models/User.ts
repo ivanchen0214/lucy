72d2340a0e462329522483977d34c2b3ce8b88f9
chenjiahau <ivan.chen@dlinkcorp.com>
2020-09-04 20:29:45 +0800

    Change

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,5 @@
+import { AxiosResponse } from 'axios';
+
 import { Eventing } from './Eventing';
 import { Sync } from './Sync';
 import { Attributes } from './Attributes';
@@ -17,5 +19,45 @@ export class User {
 
   constructor(attrs: UserProp) {
     this.attributes = new Attributes<UserProp>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProp): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.get('id');
+
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without an id');
+    }
+
+    this.sync.fetch(id)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      })
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((Response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      // .catch(() => {
+      //   this.trigger('error');
+      // });
   }
 }
