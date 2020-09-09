1c25c0a58811577fe8d8b4faa4bf1c5845dd42d4
chenjiahau <ivan.chen@dlinkcorp.com>
2020-09-09 09:10:41 +0800

    Change

--- src/Routes/LoginRoutes.ts
+++ src/Routes/LoginRoutes.ts
@@ -1,13 +1,38 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted!');
+}
+
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('Hi there!');
+  console.log(req.session);
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div> You're logged in!</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `)
+  } else {
+    res.send(`
+      <div>
+        <div> You're not logged in!</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
 });
 
 router.get('/login', (req: Request, res: Response) => {
@@ -35,6 +60,15 @@ router.post('/login', (req: Request, res: Response) => {
   } else {
     res.send("Invalid email or password!");
   }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = null;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user!');
 });
 
 export { router };
